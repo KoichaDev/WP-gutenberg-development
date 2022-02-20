167a0a9149ebcb80f59882943fa65a21a7d8cee8
“Khoi <khoi@godtsagt.no>
2022-02-20 22:47:53 +0100

    Add: ✨ Creating a new WordPress plugin (First Block)

---
+++ wp-content/plugins/first-block/first_block.php
@@ -0,0 +1,8 @@
+<?php
+/** 
+ * Plugin Name: First Block
+ * Plugin URI: http://www.example.com/
+ * Description: This is my first block.
+ * Author: Khoi Hoang
+ * Author URI: http://www.example.com/
+*/
