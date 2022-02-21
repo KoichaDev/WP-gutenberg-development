29a1841a12b1de75b1cf4b1923a686504d183113
“Khoi <khoi@godtsagt.no>
2022-02-21 21:35:33 +0100

    Update: ✨ Editing the Boilerplate's Metadata for text block

--- wp-content/plugins/text-block/src/index.js
+++ wp-content/plugins/text-block/src/index.js
@@ -3,7 +3,7 @@ import './style.scss';
 import Edit from './edit';
 import save from './save';
 
-registerBlockType('create-block/boilerplate', {
+registerBlockType('block-course/text-block', {
 	edit: Edit,
 	save,
 });
