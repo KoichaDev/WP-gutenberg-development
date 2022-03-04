64431a5d4628a42b3dc794aeb137a4d3de45142f
“Khoi <khoi@godtsagt.no>
2022-03-04 21:52:03 +0100

    Add: ✨ Demo Example how to dispatch the title using editor @wordpress/data

--- wp-content/plugins/wp-blocks-course-data-store-demo-main/src/edit.js
+++ wp-content/plugins/wp-blocks-course-data-store-demo-main/src/edit.js
@@ -4,7 +4,18 @@ import { useSelect, useDispatch } from '@wordpress/data';
 export default function Edit() {
 	const title = useSelect((select) => {
 		return select('core/editor').getEditedPostAttribute('title');
-	});
+	}, []);
+
 	const { editPost } = useDispatch('core/editor');
-	return <p></p>
+
+	return (
+		<div {...useBlockProps()}>
+			<h2>{title}</h2>
+
+			<input
+				value={title}
+				onChange={(e) => editPost({ title: e.target.value })}
+			/>
+		</div>
+	);
 }
