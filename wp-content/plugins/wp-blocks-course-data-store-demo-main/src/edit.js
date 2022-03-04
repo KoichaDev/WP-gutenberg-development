d9dbc922bbd9cd3b9d56369dd70e6bf450084a06
“Khoi <khoi@godtsagt.no>
2022-03-04 21:33:23 +0100

    Add: 📦 @wordpress/data

--- wp-content/plugins/wp-blocks-course-data-store-demo-main/src/edit.js
+++ wp-content/plugins/wp-blocks-course-data-store-demo-main/src/edit.js
@@ -6,13 +6,5 @@ export default function Edit() {
 		return select('core/editor').getEditedPostAttribute('title');
 	});
 	const { editPost } = useDispatch('core/editor');
-	return (
-		<div {...useBlockProps()}>
-			<h2>{title}</h2>
-			<input
-				value={title}
-				onChange={(e) => editPost({ title: e.target.value })}
-			/>
-		</div>
-	);
+	return <p></p>
 }
